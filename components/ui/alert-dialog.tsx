910bd4c8b3a0edb17aefa60cd4015c4aa80b410a
rod-jemeel <rodj.faburada@gmail.com>
2026-01-18 14:50:11 +0800

    style(ui): add text-balance to title components

--- components/ui/alert-dialog.tsx
+++ components/ui/alert-dialog.tsx
@@ -110,7 +110,7 @@ function AlertDialogTitle({
   return (
     <AlertDialogPrimitive.Title
       data-slot="alert-dialog-title"
-      className={cn("text-sm font-medium sm:group-data-[size=default]/alert-dialog-content:group-has-data-[slot=alert-dialog-media]/alert-dialog-content:col-start-2", className)}
+      className={cn("text-sm font-medium text-balance sm:group-data-[size=default]/alert-dialog-content:group-has-data-[slot=alert-dialog-media]/alert-dialog-content:col-start-2", className)}
       {...props}
     />
   )
